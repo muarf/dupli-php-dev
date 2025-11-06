528c15ddf49bfd579205b47a456ca68f6a206c51
muarf <muarf@riseup.net>
2025-11-06 12:34:26 -0500

    feat: ajout du titre 'Duplicator' dans base.html.php

--- view/base.html.php
+++ view/base.html.php
@@ -3,6 +3,7 @@
     <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Duplicator</title>
     <script type="text/javascript" src="js/jquery.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script type="text/javascript" src="js/calcul.js"></script>
