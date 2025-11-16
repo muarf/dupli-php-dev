0040647c1e57176f962e1956b6b70c434b790373
muarf <muarf@riseup.net>
2025-11-16 08:05:47 -0500

    debug: journalisation mots de passe et runner tests

--- download_pdf.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-require_once __DIR__ . '/api/download_pdf.php';
--- tests/Feature/ApiTamboursTest.php
+++ tests/Feature/ApiTamboursTest.php
@@ -59,7 +59,7 @@ it('met à jour la liste des tambours et leurs prix', function () {
 
 function run_ajax_get_tambour_prices(array $post, array $conf): string
 {
-    return execute_api_script('ajax_get_tambour_prices.php', [
+    return execute_routed_endpoint('ajax_get_tambour_prices', [
         'method' => 'POST',
         'post' => $post,
         'session' => ['user' => 'admin'],
@@ -70,7 +70,7 @@ function run_ajax_get_tambour_prices(array $post, array $conf): string
 
 function run_ajax_edit_tambours(array $post, array $conf): string
 {
-    return execute_api_script('ajax_edit_tambours.php', [
+    return execute_routed_endpoint('ajax_edit_tambours', [
         'method' => 'POST',
         'post' => $post,
         'session' => ['user' => 'admin'],
@@ -79,14 +79,13 @@ function run_ajax_edit_tambours(array $post, array $conf): string
     ]);
 }
 
-function execute_api_script(string $scriptName, array $config): string
+function execute_routed_endpoint(string $endpoint, array $config): string
 {
-    $runner = realpath(__DIR__ . '/../helpers/run_endpoint.php');
-    $script = realpath(__DIR__ . '/../../api/' . $scriptName);
+    $runner = realpath(__DIR__ . '/../helpers/run_routed_endpoint.php');
 
     $command = escapeshellarg(PHP_BINARY) . ' ' .
         escapeshellarg($runner) . ' ' .
-        escapeshellarg($script) . ' ' .
+        escapeshellarg($endpoint) . ' ' .
         escapeshellarg(json_encode($config));
 
     $output = [];
