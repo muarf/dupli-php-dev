1ad3fabcf947a446899bdab968c9a919d1c686cd
muarf <muarf@users.noreply.github.com>
2025-12-11 13:09:31 +0100

    Restore files lost during stash pop (from 9a7ec95)

---
+++ models/admin.imprimantes.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Modèle pour la page d'administration du moniteur d'imprimantes
+ */
+
+function Action($conf = null)
+{
+    // Cette page ne nécessite pas de traitement particulier
+    // Elle utilise l'API Electron et les endpoints PHP pour afficher les données
+    return [];
+}
+
